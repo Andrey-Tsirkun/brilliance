41c264ef6666dabfeb9cf105c75e20ed80472444
Andrey-Tsirkun <n-drupal@tut.by>
2015-07-06 23:35:10 +0300

    06-07-2015

--- sites/all/themes/brilliance/templates/page.tpl.php
+++ sites/all/themes/brilliance/templates/page.tpl.php
@@ -66,10 +66,6 @@
 
         <div class="page-top-wrapper <?php /*print $page_top_regions_class;*/ ?>">
 
-          <?php if ($breadcrumb): ?>
-            <div class="breadcrumb"><?php print $breadcrumb; ?></div>
-          <?php endif; ?>
-
           <?php if ($page['page_top_first']): ?>
             <?php print render($page['page_top_first']); ?>
           <?php endif; ?>
@@ -93,6 +89,9 @@
               <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
               <a id="main-content"></a>
               <?php print $messages; ?>
+              <?php if ($breadcrumb): ?>
+                <?php print $breadcrumb; ?>
+              <?php endif; ?>
               <?php print render($title_prefix); ?>
               <?php if ($title): ?><h1 class="title page-title"><?php print $title; ?></h1><?php endif; ?>
               <?php print render($title_suffix); ?>
@@ -146,5 +145,7 @@
         </div>
       <?php endif; ?>
     </footer> <!-- /.section, /#footer -->
+
+    <div class="brilliance-slider123"><?php print $slider; ?></div>
 
   </div> <!-- /#page-wrapper -->
--- sites/all/themes/brilliance/theme-settings.php
+++ sites/all/themes/brilliance/theme-settings.php
@@ -75,4 +75,277 @@ function brilliance_form_system_theme_settings_alter(&$form, &$form_state) {
       '#default_value' => theme_get_setting('instagram_url', 'brilliance'),
       '#description' => t('Enter your Instagram profile URL.'),
   );
+
+  //Slider
+
+  $form['brilliance_settings']['brilliance_slider'] = array(
+      '#type' => 'fieldset',
+      '#title' => t('Brilliance slider'),
+      '#collapsible' => TRUE,
+      '#collapsed' => TRUE,
+  );
+
+ /*$form['brilliance_settings']['brilliance_slider']['slides_count'] = array(
+      '#title' => 'Number of slides',
+      '#type' => 'textfield',
+      '#description' => t('Enter the total number of images in the slideshow.'),
+      '#default_value' => theme_get_setting('slides_count','brilliance'),
+      '#size' => 3,
+  );
+
+  for ($i = 0; $i < theme_get_setting('slides_count'); $i++){
+    $form['brilliance_settings']['brilliance_slider']['slide_' . $i] = array(
+        '#type' => 'fieldset',
+        '#title' => t('Slide') . ' #'.($i+1),
+        '#collapsible' => TRUE,
+        '#collapsed' => TRUE,
+    );
+    $form['brilliance_settings']['brilliance_slider']['slide_' . $i]['slide_image' . $i] = array(
+        '#type' => 'managed_file',
+        '#title' => t('Slide image'),
+        '#required' => TRUE,
+        '#upload_location' => 'public://slider/',
+        '#default_value' => theme_get_setting('slide_image' . $i, 'brilliance'),
+        '#upload_validators' => array(
+            'file_validate_extensions' => array('gif png jpg jpeg'),
+            'file_validate_image_resolution' => array('1140x300')
+        ),
+    );
+    $form['brilliance_settings']['brilliance_slider']['slide_' . $i]['slide_title' . $i] = array(
+        '#type' => 'textfield',
+        '#title' => t('Slide title'),
+        '#description' => t('Enter title for current slide.'),
+        '#default_value' => theme_get_setting('slide_title' . $i, 'brilliance'),
+    );
+    $form['brilliance_settings']['brilliance_slider']['slide_' . $i]['slide_description' . $i] = array(
+        '#type' => 'textarea',
+        '#title' => t('Slide description'),
+        '#description' => t('Enter description for current slide.'),
+        '#default_value' => theme_get_setting('slide_description' . $i, 'brilliance'),
+    );
+  }*/
+
+  $form['slide'] = array(
+      '#type' => 'fieldset',
+      '#title' => t('Slide managment [Upload images with dimension 960*502]'),
+      '#weight' => 1,
+      '#collapsible' => TRUE,
+      '#collapsed' => FALSE,
+  );
+
+  // Image upload section ======================================================
+  $banners = brilliance_get_banners();
+
+  $form['slide']['images'] = array(
+      '#type' => 'vertical_tabs',
+      '#title' => t('Slide images'),
+      '#weight' => 2,
+      '#collapsible' => TRUE,
+      '#collapsed' => FALSE,
+      '#tree' => TRUE,
+  );
+
+  $i = 0;
+  foreach ($banners as $image_data) {
+    $form['slide']['images'][$i] = array(
+        '#type' => 'fieldset',
+        '#title' => t('Image !number', array('!number' => $i + 1)),
+        '#weight' => $i,
+        '#collapsible' => TRUE,
+        '#collapsed' => FALSE,
+        '#tree' => TRUE,
+      // Add image config form to $form
+        'image' => _brilliance_banner_form($image_data),
+    );
+
+    $i++;
+  }
+
+  $form['slide']['image_upload'] = array(
+      '#type' => 'file',
+      '#title' => t('Upload a new slide'),
+      '#weight' => $i,
+  );
+
+  $form['#submit'][]   = 'brilliance_settings_submit';
+  return $form;
+}
+
+function brilliance_get_banners($all = TRUE) {
+  // Get all banners
+  $slides = variable_get('theme_brilliance_banner_settings', array());
+  $delay = theme_get_setting('slide_delay');
+  $animation_speed = theme_get_setting('animation_speed');
+  $caption_animation_speed = theme_get_setting('caption_animation_speed');
+  drupal_add_js('var delay = "' . $delay . '"', 'inline');
+  drupal_add_js('var animation_speed = "' . $animation_speed . '"', 'inline');
+  drupal_add_js('var caption_animation_speed  = "'. $caption_animation_speed . '"', 'inline');
+  // Create list of banner to return
+  $slides_value = array();
+  foreach ($slides as $slide) {
+    $slide['weight'] = $slide['image_weight'];
+    $slides_value[] = $slide;
+  }
+
+  // Sort image by weight
+  usort($slides_value, 'drupal_sort_weight');
+
+  return $slides_value;
+}
+
+function _brilliance_install() {
+  // Default data
+  $file = new stdClass;
+  $slides = array();
+  // Source base for images
+  $src_base_path = drupal_get_path('theme', 'brilliance');
+  $default_slides = theme_get_setting('default_slides');
+  // Put all image as banners
+  foreach ($default_slides as $i => $data) {
+    $file -> uri = $src_base_path . '/' . $data['image_path'];
+    $file -> filename = $file->uri;
+    $slide = _brilliance_save_image($file);
+    unset($data['image_path']);
+    $slide = array_merge($slide, $data);
+    $slides[$i] = $slide;
+  }
+  // Save banner data
+  brilliance_set_banners($slides);
+  // Flag theme is installed
+  variable_set('theme_brilliance_first_install', FALSE);
+}
+
+function _brilliance_banner_form($image_data) {
+  $img_form = array();
+
+  // Image preview
+  $img_form['image_preview'] = array(
+      '#markup' => theme('image', array('path' => $image_data['image_thumb'])),
+  );
+
+  // Image path
+  $img_form['image_path'] = array(
+      '#type' => 'hidden',
+      '#value' => $image_data['image_path'],
+  );
+
+  // Thumbnail path
+  $img_form['image_thumb'] = array(
+      '#type' => 'hidden',
+      '#value' => $image_data['image_thumb'],
+  );
+
+
+  // Link url
+  $img_form['image_url'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Url'),
+      '#default_value' => $image_data['image_url'],
+  );
+
+  // Delete image
+  $img_form['image_delete'] = array(
+      '#type' => 'checkbox',
+      '#title' => t('Delete image.'),
+      '#default_value' => FALSE,
+  );
+
+  return $img_form;
+}
+
+function brilliance_settings_submit($form, &$form_state) {
+  $settings = array();
+
+  // Update image field
+  foreach ($form_state['input']['images'] as $image) {
+    if (is_array($image)) {
+      $image = $image['image'];
+
+      if ($image['image_delete']) {
+        // Delete banner file
+        file_unmanaged_delete($image['image_path']);
+        // Delete banner thumbnail file
+        file_unmanaged_delete($image['image_thumb']);
+      } else {
+        // Update image
+        $settings[] = $image;
+      }
+    }
+  }
+
+  // Check for a new uploaded file, and use that if available.
+  if ($file = file_save_upload('image_upload')) {
+    $file -> status = FILE_STATUS_PERMANENT;
+    if ($image = _brilliance_save_image($file)) {
+      // Put new image into settings
+      $settings[] = $image;
+    }
+  }
+
+  // Save settings
+  brilliance_set_banners($settings);
+}
+
+/**
+ * Save file uploaded by user and generate setting to save.
+ *
+ * @param  $file
+ *    File uploaded from user
+ *
+ * @param  $banner_folder
+ *    Folder where save image
+ *
+ * @param  $banner_thumb_folder
+ *    Folder where save image thumbnail
+ *
+ * @return
+ *    Array with file data.
+ *    FALSE on error.
+ */
+function _brilliance_save_image($file, $slide_folder = 'public://brilliance-slider/', $slide_thumb_folder = 'public://brilliance-slider/thumb/') {
+  // Check directory and create it (if not exist)
+  _brilliance_check_dir($slide_folder);
+  _brilliance_check_dir($slide_thumb_folder);
+
+  $parts = pathinfo($file->filename);
+  $destination = $slide_folder . $parts['basename'];
+  $setting = array();
+
+  $file -> status = FILE_STATUS_PERMANENT;
+
+  // Copy temporary image into banner folder
+  if ($img = file_copy($file, $destination, FILE_EXISTS_REPLACE)) {
+    // Generate image thumb
+    $image = image_load($destination);
+    $small_img = image_scale($image, 300, 100);
+    $image->source = $slide_thumb_folder . $parts['basename'];
+    image_save($image);
+
+    // Set image info
+    $setting['image_path'] = $destination;
+    $setting['image_thumb'] = $image->source;
+    $setting['image_url'] = '';
+    $setting['image_visibility'] = '*';
+    return $setting;
+  }
+
+  return FALSE;
+}
+
+function brilliance_set_banners($value) {
+  variable_set('theme_brilliance_banner_settings', $value);
+}
+
+/**
+ * Check if folder is available or create it.
+ *
+ * @param  $dir
+ *    Folder to check
+ */
+function _brilliance_check_dir($dir) {
+  // Normalize directory name
+  $dir = file_stream_wrapper_uri_normalize($dir);
+
+  // Create directory (if not exist)
+  file_prepare_directory($dir,  FILE_CREATE_DIRECTORY);
 }
