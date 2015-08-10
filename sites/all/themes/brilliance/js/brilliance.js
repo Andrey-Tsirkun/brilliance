e8f6b38afbd2ab535cbabcd38c26ae8ae5acb812
Andrey-Tsirkun <n-drupal@tut.by>
2015-08-10 21:53:25 +0300

    10-08-2015

--- sites/all/themes/brilliance/js/brilliance.js
+++ sites/all/themes/brilliance/js/brilliance.js
@@ -1,64 +1,65 @@
 (function ($) {
-  if (Drupal.settings.navFixed) {
-    var nav = $('.navigation'),
-        logo = $('.logo'),
-        menuBlock = $('.navigation .block-menu'),
-        homeLink = '<a href="'
-            + Drupal.settings.homeLinkURL +
-            '" class="homelink" title="'
-            + Drupal.t('Home') + '">'
-            + Drupal.t('Home') + '</a>';
-    stickyRibbonTop = nav.offset().top;
-    menuBlock.before(homeLink);
-    $(window).scroll(function () {
-      if (($(window).scrollTop() > stickyRibbonTop) && $(window).width() > 768) {
-        nav.addClass('fixedNav');
-      } else {
-        nav.removeClass('fixedNav');
-      }
-      logo.css('top', -$(window).scrollTop());
-    });
-  }
-
-  var mobileMenuButton = $('.mobileMenuButton'),
-      navigation = $('.navigation');
-
-  mobileMenuButton.click(function () {
-    navigation.toggleClass('active');
-  });
-
-  var menuLink = $('.expanded > a'),
-      menuInnerList = menuLink.siblings('ul');
-
-  //Remove click event for elements with dropdown children
-  menuLink.click(false);
-  menuLink.addClass('ololo');
-
-  function menuBehaviour() {
-    if ($(window).width() <= 767) {
-      menuLink.click(function () {
-        $(this).toggleClass('activeUl');
-        $(this).unbind('mouseenter mouseleave');
-        $(this).siblings('ul').slideToggle(200);
-      });
-    }
-    if ($(window).width() > 768) {
-      menuInnerList.hover(function () {
-        $(this).siblings('a').toggleClass('activeUl');
-      });
-    }
-  }
-
-  menuBehaviour();
-
-  $(window).resize(function () {
-    menuBehaviour();
-  });
-
-  $('.brilliance-slider').brillianceSlider();
-
-  Drupal.behaviors.brilliance = {
+  Drupal.behaviors.brilliancslider = {
     attach: function (context, settings) {
+
+      if (Drupal.settings.navFixed) {
+        var nav = $('.navigation'),
+            logo = $('.logo'),
+            menuBlock = $('.navigation .block-menu'),
+            homeLink = '<a href="'
+                + Drupal.settings.homeLinkURL +
+                '" class="homelink" title="'
+                + Drupal.t('Home') + '">'
+                + Drupal.t('Home') + '</a>';
+        stickyRibbonTop = nav.offset().top;
+        menuBlock.before(homeLink);
+        $(window).scroll(function () {
+          if (($(window).scrollTop() > stickyRibbonTop) && $(window).width() > 768) {
+            nav.addClass('fixedNav');
+          } else {
+            nav.removeClass('fixedNav');
+          }
+          logo.css('top', -$(window).scrollTop());
+        });
+      }
+
+      var mobileMenuButton = $('.mobileMenuButton'),
+          navigation = $('.navigation');
+
+      mobileMenuButton.click(function () {
+        navigation.toggleClass('active');
+      });
+
+      var menuLink = $('.expanded > a'),
+          menuInnerList = menuLink.siblings('ul');
+
+      //Remove click event for elements with dropdown children
+      menuLink.click(false);
+      menuLink.addClass('ololo');
+
+      function menuBehaviour() {
+        if ($(window).width() <= 767) {
+          menuLink.click(function () {
+            $(this).toggleClass('activeUl');
+            $(this).unbind('mouseenter mouseleave');
+            $(this).siblings('ul').slideToggle(200);
+          });
+        }
+        if ($(window).width() > 768) {
+          menuInnerList.hover(function () {
+            $(this).siblings('a').toggleClass('activeUl');
+          });
+        }
+      }
+
+      menuBehaviour();
+
+      $(window).resize(function () {
+        menuBehaviour();
+      });
+
+      $('.brilliance-slider').brillianceSlider();
+
 
       // Здесь указываем ID блока с логином.
       var $loginBlock = $('#block-user-login');
